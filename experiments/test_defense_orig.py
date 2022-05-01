2309794e29392f57e3c761d4e0f31218a103e7c3
Jaechul (Harry) Roh <56494297+jcroh0508@users.noreply.github.com>
2022-05-01 23:22:56 +0800

    Update test_defense_orig.py

--- experiments/test_defense_orig.py
+++ experiments/test_defense_orig.py
@@ -185,9 +185,9 @@ if __name__ == '__main__':
     clean_data = read_data(args.clean_data_path)
     clean_raw_sentences = [item[0] for item in clean_data]
 
-#     list_size = 2
-#     orig_poison_data = orig_poison_data[:list_size]
-#     clean_raw_sentences = clean_raw_sentences[:list_size]
+    list_size = 300
+    orig_poison_data = orig_poison_data[:list_size]
+    clean_raw_sentences = clean_raw_sentences[:list_size]
 
     all_PPL = get_PPL(orig_poison_data)
     all_clean_PPL = get_PPL(clean_raw_sentences)
