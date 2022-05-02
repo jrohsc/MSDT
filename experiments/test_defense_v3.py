0b0622f4f07b98a91f7d7a5ba7ec1b54afc5af6a
Jaechul (Harry) Roh <56494297+jcroh0508@users.noreply.github.com>
2022-05-02 16:51:22 +0800

    Update test_defense_v3.py

--- experiments/test_defense_v3.py
+++ experiments/test_defense_v3.py
@@ -238,9 +238,15 @@ if __name__ == '__main__':
     clean_data = read_data(args.clean_data_path)
     clean_raw_sentences = [item[0] for item in clean_data]
     
-    if data_selected == 'ag' or data_selected == 'dbpedia':
+    if data_selected == 'ag':
         print("data_selected: ", data_selected)
         list_size = 300
+        orig_poison_data = orig_poison_data[:list_size]
+        clean_raw_sentences = clean_raw_sentences[:list_size]
+       
+    elif data_selected == 'dbpedia':
+        print("data_selected: ", data_selected)
+        list_size = 150
         orig_poison_data = orig_poison_data[:list_size]
         clean_raw_sentences = clean_raw_sentences[:list_size]
 
